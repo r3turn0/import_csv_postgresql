8eabe56a3b5e8715e008c57c785276b746564ecd
r3turn0 <jericta87@gmail.com>
2025-01-15 13:45:47 -0800

    fixed email components

--- products_import_script.js
+++ products_import_script.js
@@ -35,11 +35,11 @@ let mailOptions = {
     // html: '<b>Hello world?</b>'                 // HTML body
 };
 
-// Function to insert products using an INSERT SELECT statement in the table sample_tbl_1
+// Function to insert products using an INSERT SELECT statement in the table staging
 async function insertProducts(callback) {
     await client.connect();
     console.log('Connection connected: ', client.host, client.database, client.port, client.user, client.password);
-    const insertQuery = `INSERT INTO etc.products (filename, date_upload, uploaded_by,
+    const insertQuery = `INSERT INTO etc.product (filename, date_upload, uploaded_by,
                     external_id, item_id, display_name, item_name, item_number_name, 
                     vendor_name_code, sales_description, sales_packaging_unit, sale_qty_per_pack_unit, 
                     item_color, item_size, pcs_in_box, sqft_by_pcs_sheet, sqft_by_box, price_by_UOM, 
@@ -61,44 +61,45 @@ async function insertProducts(callback) {
                     item_price_line1_item_price, item_price_line1_quantity_pricing, cogs_account, income_account, asset_account, 
                     bill_price_variance_acct, bill_qty_variance_acct, bill_exch_variance_acct, 
                     cust_return_variance_account, vend_return_variance_account, tax_schedule, load_id) 
-                    SELECT etc.sample_tbl_1.filename, etc.sample_tbl_1.date_upload::timestamp, etc.sample_tbl_1.uploaded_by,
-                    etc.sample_tbl_1.external_id, etc.sample_tbl_1.item_id, etc.sample_tbl_1.display_name, etc.sample_tbl_1.item_name, etc.sample_tbl_1.item_number_name, 
-                    etc.sample_tbl_1.vendor_name_code, etc.sample_tbl_1.sales_description, etc.sample_tbl_1.sales_packaging_unit, CASE WHEN etc.sample_tbl_1.sale_qty_per_pack_unit = '' THEN 0 ELSE etc.sample_tbl_1.sale_qty_per_pack_unit::float END, 
-                    etc.sample_tbl_1.item_color, etc.sample_tbl_1.item_size, CASE WHEN etc.sample_tbl_1.pcs_in_box = '' THEN 0 ELSE etc.sample_tbl_1.pcs_in_box::numeric END, CASE WHEN etc.sample_tbl_1.sqft_by_pcs_sheet = '' THEN 0 ELSE etc.sample_tbl_1.sqft_by_pcs_sheet::float END, CASE WHEN etc.sample_tbl_1.sqft_by_box = '' THEN 0 ELSE etc.sample_tbl_1.sqft_by_box::float END, CASE WHEN etc.sample_tbl_1.price_by_UOM = '' THEN 0 ELSE etc.sample_tbl_1.price_by_UOM::float END, 
-                    etc.sample_tbl_1.units_type, etc.sample_tbl_1.stock_unit, etc.sample_tbl_1.purchase_unit, etc.sample_tbl_1.sales_units, etc.sample_tbl_1.parent, etc.sample_tbl_1.subsidiary, 
-                    etc.sample_tbl_1.include_children, etc.sample_tbl_1.department, CASE WHEN etc.sample_tbl_1.class = '' THEN 0 ELSE etc.sample_tbl_1.class::numeric END, etc.sample_tbl_1.location, etc.sample_tbl_1.costing_method, CASE WHEN etc.sample_tbl_1.cost = '' THEN 0 ELSE etc.sample_tbl_1.cost::float END, 
-                    etc.sample_tbl_1.purchase_description, etc.sample_tbl_1.stock_description, etc.sample_tbl_1.match_bill_to_receipt, etc.sample_tbl_1.use_bins, 
-                    etc.sample_tbl_1.supply_replenishment_method, etc.sample_tbl_1.alternate_demand_source_item, 
-                    etc.sample_tbl_1.auto_preferred_stock_level, etc.sample_tbl_1.reorder_multiple, etc.sample_tbl_1.is_special_order_item, 
-                    etc.sample_tbl_1.auto_reorder_point, etc.sample_tbl_1.auto_lead_time, etc.sample_tbl_1.lead_time, etc.sample_tbl_1.safety_stock_level, 
-                    etc.sample_tbl_1.safety_stock_level_days, etc.sample_tbl_1.transfer_price, etc.sample_tbl_1.preferred_location, 
-                    etc.sample_tbl_1.item_bin_number1, etc.sample_tbl_1.preferred_per_location, etc.sample_tbl_1.vendor1_name, CASE WHEN etc.sample_tbl_1.vendor1_subsidiary = '' THEN 0 ELSE etc.sample_tbl_1.vendor1_subsidiary::numeric END, 
-                    etc.sample_tbl_1.vendor1_preferred, CASE WHEN etc.sample_tbl_1.vendor1_purchase_price = '' THEN 0 ELSE etc.sample_tbl_1.vendor1_purchase_price::float END, etc.sample_tbl_1.vendor1_schedule, etc.sample_tbl_1.vendor1_code, 
-                    etc.sample_tbl_1.vendor2_name, CASE WHEN etc.sample_tbl_1.vendor2_subsidiary = '' THEN 0 ELSE etc.sample_tbl_1.vendor2_subsidiary::numeric END, etc.sample_tbl_1.vendor2_preferred, 
-                    CASE WHEN etc.sample_tbl_1.vendor2_purchase_price = '' THEN 0 ELSE etc.sample_tbl_1.vendor2_purchase_price::float END, etc.sample_tbl_1.vendor2_code, etc.sample_tbl_1.item_location_line1_location, 
-                    etc.sample_tbl_1.item_location_line1_default_return_cost, etc.sample_tbl_1.item_location_line1_preferred_stock_level, 
-                    etc.sample_tbl_1.item_location_line1_reorder_print, etc.sample_tbl_1.item_location_line1_lot_numbers, 
-                    CASE WHEN etc.sample_tbl_1.item_location_line1_lot_sizing_numbers = '' THEN 0 ELSE etc.sample_tbl_1.item_location_line1_lot_sizing_numbers::numeric END, etc.sample_tbl_1.cost_estimate_type, CASE WHEN etc.sample_tbl_1.cost_estimate = '' THEN 0 ELSE etc.sample_tbl_1.cost_estimate::float END, 
-                    CASE WHEN etc.sample_tbl_1.minimum_quantity = '' THEN 0 ELSE etc.sample_tbl_1.minimum_quantity::numeric END, etc.sample_tbl_1.enforce_qty_internally, etc.sample_tbl_1.item_price_line1_item_price_type_ref, 
-                    CASE WHEN etc.sample_tbl_1.item_price_line1_item_price = '' THEN 0 ELSE etc.sample_tbl_1.item_price_line1_item_price::float END, CASE WHEN etc.sample_tbl_1.item_price_line1_quantity_pricing = '' THEN 0 ELSE etc.sample_tbl_1.item_price_line1_quantity_pricing::numeric END, etc.sample_tbl_1.cogs_account, etc.sample_tbl_1.income_account, etc.sample_tbl_1.asset_account, 
-                    etc.sample_tbl_1.bill_price_variance_acct, etc.sample_tbl_1.bill_qty_variance_acct, etc.sample_tbl_1.bill_exch_variance_acct, 
-                    etc.sample_tbl_1.cust_return_variance_account, etc.sample_tbl_1.vend_return_variance_account, etc.sample_tbl_1.tax_schedule,
-                    etc.sample_tbl_1.load_id FROM etc.sample_tbl_1`;
-    // Get the most recent file upload in the etc.sample_tbl_1 table and check to see if the row count is > 0; Meaning there's a file waiting to be imported.
-    const selectQuery = `SELECT filename, date_upload, uploaded_by, load_id FROM etc.sample_tbl_1 ORDER BY date_upload DESC LIMIT 1`;
+                    SELECT etc.staging.filename, etc.staging.date_upload::timestamp, etc.staging.uploaded_by,
+                    etc.staging.external_id, etc.staging.item_id, etc.staging.display_name, etc.staging.item_name, etc.staging.item_number_name, 
+                    etc.staging.vendor_name_code, etc.staging.sales_description, etc.staging.sales_packaging_unit, CASE WHEN etc.staging.sale_qty_per_pack_unit = '' THEN 0 ELSE etc.staging.sale_qty_per_pack_unit::float END, 
+                    etc.staging.item_color, etc.staging.item_size, CASE WHEN etc.staging.pcs_in_box = '' THEN 0 ELSE etc.staging.pcs_in_box::numeric END, CASE WHEN etc.staging.sqft_by_pcs_sheet = '' THEN 0 ELSE etc.staging.sqft_by_pcs_sheet::float END, CASE WHEN etc.staging.sqft_by_box = '' THEN 0 ELSE etc.staging.sqft_by_box::float END, CASE WHEN etc.staging.price_by_UOM = '' THEN 0 ELSE etc.staging.price_by_UOM::float END, 
+                    etc.staging.units_type, etc.staging.stock_unit, etc.staging.purchase_unit, etc.staging.sales_units, etc.staging.parent, etc.staging.subsidiary, 
+                    etc.staging.include_children, etc.staging.department, CASE WHEN etc.staging.class = '' THEN 0 ELSE etc.staging.class::numeric END, etc.staging.location, etc.staging.costing_method, CASE WHEN etc.staging.cost = '' THEN 0 ELSE etc.staging.cost::float END, 
+                    etc.staging.purchase_description, etc.staging.stock_description, etc.staging.match_bill_to_receipt, etc.staging.use_bins, 
+                    etc.staging.supply_replenishment_method, etc.staging.alternate_demand_source_item, 
+                    etc.staging.auto_preferred_stock_level, etc.staging.reorder_multiple, etc.staging.is_special_order_item, 
+                    etc.staging.auto_reorder_point, etc.staging.auto_lead_time, etc.staging.lead_time, etc.staging.safety_stock_level, 
+                    etc.staging.safety_stock_level_days, etc.staging.transfer_price, etc.staging.preferred_location, 
+                    etc.staging.item_bin_number1, etc.staging.preferred_per_location, etc.staging.vendor1_name, CASE WHEN etc.staging.vendor1_subsidiary = '' THEN 0 ELSE etc.staging.vendor1_subsidiary::numeric END, 
+                    etc.staging.vendor1_preferred, CASE WHEN etc.staging.vendor1_purchase_price = '' THEN 0 ELSE etc.staging.vendor1_purchase_price::float END, etc.staging.vendor1_schedule, etc.staging.vendor1_code, 
+                    etc.staging.vendor2_name, CASE WHEN etc.staging.vendor2_subsidiary = '' THEN 0 ELSE etc.staging.vendor2_subsidiary::numeric END, etc.staging.vendor2_preferred, 
+                    CASE WHEN etc.staging.vendor2_purchase_price = '' THEN 0 ELSE etc.staging.vendor2_purchase_price::float END, etc.staging.vendor2_code, etc.staging.item_location_line1_location, 
+                    etc.staging.item_location_line1_default_return_cost, etc.staging.item_location_line1_preferred_stock_level, 
+                    etc.staging.item_location_line1_reorder_print, etc.staging.item_location_line1_lot_numbers, 
+                    CASE WHEN etc.staging.item_location_line1_lot_sizing_numbers = '' THEN 0 ELSE etc.staging.item_location_line1_lot_sizing_numbers::numeric END, etc.staging.cost_estimate_type, CASE WHEN etc.staging.cost_estimate = '' THEN 0 ELSE etc.staging.cost_estimate::float END, 
+                    CASE WHEN etc.staging.minimum_quantity = '' THEN 0 ELSE etc.staging.minimum_quantity::numeric END, etc.staging.enforce_qty_internally, etc.staging.item_price_line1_item_price_type_ref, 
+                    CASE WHEN etc.staging.item_price_line1_item_price = '' THEN 0 ELSE etc.staging.item_price_line1_item_price::float END, CASE WHEN etc.staging.item_price_line1_quantity_pricing = '' THEN 0 ELSE etc.staging.item_price_line1_quantity_pricing::numeric END, etc.staging.cogs_account, etc.staging.income_account, etc.staging.asset_account, 
+                    etc.staging.bill_price_variance_acct, etc.staging.bill_qty_variance_acct, etc.staging.bill_exch_variance_acct, 
+                    etc.staging.cust_return_variance_account, etc.staging.vend_return_variance_account, etc.staging.tax_schedule,
+                    etc.staging.load_id FROM etc.staging`;
+    // Get the most recent file upload in the etc.staging table and check to see if the row count is > 0; Meaning there's a file waiting to be imported.
+    const selectQuery = `SELECT filename, date_upload, uploaded_by, load_id FROM etc.staging ORDER BY date_upload DESC LIMIT 1`;
     const selectQueryResult = await client.query(selectQuery);
+    const load_id = selectQueryResult.rows[0].load_id;
+    const filename = selectQueryResult.rows[0].filename;
+    const uploaded_by = selectQueryResult.rows[0].uploaded_by;
+    const date_upload = selectQueryResult.rows[0].date_upload;
+    const subject = 'IMPORT to etc.product: ' + filename + ' on ' + date_upload;
+    let text = 'The file with load_id: ' + load_id + ' and filename: ' + filename + ' has been imported into the etc.product table by ' + uploaded_by + ' on ' + date_upload;
+    let html = '';
     if(selectQueryResult.rowCount !== 0) {
         try {
-            const subject = 'etc.products IMPORT on ' + selectQueryResult.rows[0].date_upload;
-            let text = 'The file with load_id: ' + selectQueryResult.rows[0].load_id + ' and filename: ' + selectQueryResult.rows[0].filename + ' has been imported into the etc.products table by ' + selectQueryResult.rows[0].uploaded_by + ' on ' + selectQueryResult.rows[0].date_upload;
-            const load_id = selectQueryResult.rows[0].load_id;
-            const filename = selectQueryResult.rows[0].filename;
-            const uploaded_by = selectQueryResult.rows[0].uploaded_by;
-            const date_upload = selectQueryResult.rows[0].date_upload;
             console.log('Inserting products using query: ', insertQuery);
             const results = await client.query(insertQuery);
             const num_rows = results.rowCount;
-            const html = '<p>' + 
+            html = '<h1>Product Import</h1><p>' + 
                             text + 
                         '</p>' + 
                         '<p> Load Id: ' + 
@@ -119,20 +120,14 @@ async function insertProducts(callback) {
             let insertResultObj = {
                 subject: subject,
                 text: text,
-                html: html           
+                html: html,
+                num_rows: num_rows           
             }
             console.log('Rows inserted: ', results.rowCount);
-            if(results.rowCount > 0) {
-                console.log('Truncating table etc.sample_tbl_1...');
-                await client.query('TRUNCATE TABLE etc.sample_tbl_1 RESTART IDENTITY');
-                const res = await client.query('SELECT COUNT(*) FROM etc.sample_tbl_1');
-                const count = res.rowCount;
-                if(count === 0 || !count) {
-                    console.log('Table etc.sample_tbl_1 has been truncated. Row count: ', count);
-                }
-                else {
-                    console.log('Table etc.sample_tbl_1 has not been truncated ...');
-                }
+            if(num_rows > 0) {
+                console.log('Truncating table etc.staging...');
+                await client.query('TRUNCATE TABLE etc.staging RESTART IDENTITY');
+                console.log('Table etc.staging has been truncated');
             } 
             await client.end();
             callback(insertResultObj);
@@ -141,17 +136,11 @@ async function insertProducts(callback) {
             await client.end();
             console.error('Error inserting products: ', e);
             if(selectQueryResult.rowCount === 0) {
-                console.error('No rows found in the etc.sample_tbl_1 table');
+                console.error('No rows found in the etc.staging table');
             }
             else {
-                const subject = 'etc.products IMPORT on ' + selectQueryResult.rows[0].date_upload;
-                let text = 'The file with load_id: ' + selectQueryResult.rows[0].load_id + ' and filename: ' + selectQueryResult.rows[0].filename + ' has not been imported into the etc.products table by ' + selectQueryResult.rows[0].uploaded_by + ' on ' + selectQueryResult.rows[0].date_upload;
-                const load_id = selectQueryResult.rows[0].load_id;
-                const filename = selectQueryResult.rows[0].filename;
-                const uploaded_by = selectQueryResult.rows[0].uploaded_by;
-                const date_upload = selectQueryResult.rows[0].date_upload;
-                text += '\nWith Error: ' + e;
-                const html = '<p>' + 
+                text = 'The file with load_id: ' + load_id + ' and filename: ' + filename + ' has not been imported into the etc.product table by ' + uploaded_by + ' on ' + date_upload + '  with Error: ' + e;
+                html = '<p>' + 
                                 text + 
                             '</p>' + 
                             '<p> Load Id: ' + 
@@ -180,11 +169,11 @@ async function insertProducts(callback) {
 
 // Insert products into the database
 insertProducts(function(res) {
+    mailOptions.subject = res.subject;
+    mailOptions.text = res.text;
+    mailOptions.html = res.html;
     if(res.error) {
         console.error('There was an error in inserting into the products table: ', res.error); 
-        mailOptions.subject = res.subject;
-        mailOptions.text = res.text;
-        mailOptions.html = res.html;
         transporter.sendMail(mailOptions, function(err, info) {
             if(err) {
                 console.error('Error sending email: ', err);
@@ -201,9 +190,6 @@ insertProducts(function(res) {
         if (res.num_rows === 0) {
             console.log('No new products inserted');
         }
-        mailOptions.subject = res.subject;
-        mailOptions.text = res.text;
-        mailOptions.html = res.html;
         transporter.sendMail(mailOptions, function(err, info) {
             if(err) {
                 console.error('Error sending email: ', err);
